9749573f665873efbfcfa6b363e6bd1b3e551497
qrioustechy <86207945+qrioustechy@users.noreply.github.com>
2021-07-30 16:58:13 -0400

    Added the requirements, python, setup and Procfile

---
+++ app.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jul 30 16:53:03 2021
+
+@author: od297
+"""
+import streamlit as st
+
+st.title("Data Science Career Diversity Gap")
+
+st.subheader("DS4A Team 67 App")
